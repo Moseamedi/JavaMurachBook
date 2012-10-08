6bf38249da80726cfb7dedf0125a38f3f7f295b6
David Ellinger <dellinger@ltionline.com>
2012-10-08 15:54:34 -0400

    committing changes

---
+++ netbeans/ex_starts/ch09_ex1_DisplayableTest/src/DepartmentConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dellinger
+ */
+public interface DepartmentConstants {
+    int ADMIN = 1;
+    int EDITORIAL = 2;
+    int MARKETING = 3;
+  
+    
+}
