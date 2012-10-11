6f5c6b8622c4307e2b12f0d7f1b9d4bd0e3b6c9c
David Ellinger <dellinger@ltionline.com>
2012-10-11 14:00:06 -0400

    Started making the controller

---
+++ netbeans/ex_starts/ch16_ex2_PizzaOrder/src/PizzaOrderController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+
+/**
+ *
+ * @author dellinger
+ */
+public class PizzaOrderController {
+     private javax.swing.JCheckBox chkAnchovies;
+    private javax.swing.JCheckBox chkMushrooms;
+    private javax.swing.JCheckBox chkOlives;
+    private javax.swing.JCheckBox chkPepperoni;
+    private javax.swing.JCheckBox chkSalami;
+    private javax.swing.JCheckBox chkSausage;
+    
+    double anchovies;
+    double mushrooms;
+    double olives;
+    double pepperoni;
+    
+            
+}
