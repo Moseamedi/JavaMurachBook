8bdddccc499e3ecff687365557575b61fd34ed6a
David Ellinger <dellinger@ltionline.com>
2012-10-12 15:20:06 -0400

    Worked on pizza a little more

--- netbeans/ex_starts/ch16_ex2_PizzaOrder/src/PizzaOrderController.java
+++ netbeans/ex_starts/ch16_ex2_PizzaOrder/src/PizzaOrderController.java
@@ -16,10 +16,18 @@ public class PizzaOrderController {
     private javax.swing.JCheckBox chkSalami;
     private javax.swing.JCheckBox chkSausage;
     
-    double anchovies;
-    double mushrooms;
-    double olives;
-    double pepperoni;
+    double anchovies = 1.00;
+    double mushrooms = 1.00;
+    double olives = 1.00;
+    double pepperoni = 1.00;
+    double salami = 1.50;
+    double sausage = 2.00;
+    
+    public double calculateTotalCost(){
+        
+        return 0;
+        
+    }
     
             
 }
